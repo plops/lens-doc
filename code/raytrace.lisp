b5f32f090039c6026990a573f40226ea78833d70
martin <martin@hop.kcl.ac.uk>
2011-04-18 21:14:08 +0100

    fixed problem with sphere intersection, calculate a better s, the angle for detection objective wasn't right

--- code/raytrace.lisp
+++ code/raytrace.lisp
@@ -43,14 +43,25 @@
     (the num 
       (if (<= det2 0)
 	  (error "no solution")
-	  (let* ((q (* .5s0 (+ b (* (signum b) (sqrt det2)))))
+	  (let* ((q (* -.5s0 (+ b (* (signum b) (sqrt det2)))))
 		 (aa (abs a))
 		 (aq (abs q)))
 	    (cond ((< aq eps) (values (/ q a) (/ q a)))
 		  ((< aa eps) (values (/ c q) (/ c q)))
 		  (t (values (/ q a) (/ c q)))))))))
+
 #+nil
-(quadratic-root-dist 1s0 2s0 -3s0)
+(let ((w '((0 1 -1 1)
+	   (0 2 -1 .5)
+	   (1 0 -4 (2 2))
+	   (1 2 -3 (-3 1))
+	   (3 4 -12 (-2.77 1.44)))))
+  (dolist (e w)
+    (destructuring-bind (a b c r) e
+	(format t "~a~%"
+		(list r 
+		      (multiple-value-list
+		       (quadratic-root (* 1s0 a) (* 1s0 b) (* 1s0 c))))))))
 
 (defun intersect-sphere (start dir center radius)
   (declare (type vec start dir center)
@@ -59,8 +70,15 @@
   (let* ((l (.- start center))
          (c (- (dot l l) (* radius radius)))
          (b (* 2s0 (dot l dir))))
-    (the vec (multiple-value-bind (a q)
+    (the vec (multiple-value-bind (t1 t2)
 		 (quadratic-root 1s0 b c)
 	       ;; we are only interested in the intersection in
-	       ;; forward direction of the ray
-	       (.+ start (.s (max a q) dir))))))
+	       ;; forward direction of the ray. if there are two
+	       ;; intersections in forward direction, return the
+	       ;; closest
+	       (.+ start (.s (cond ((< 0 (* t1 t2)) (min t1 t2))
+				   (t (max t1 t2)))
+			     dir))))))
+
+#+nil
+(intersect-sphere (v .1) (normalize (v 0 0 1)) (v) 2s0)
